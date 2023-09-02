81b28729f816cb30a0f5ae40b7c670302c57db13
Alex Grobovsky <a.grobovsky@gmail.com>
2023-09-02 15:33:09 +0500

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({subsets: ['latin']})
+
+export const metadata = {
+  title: 'Alexander Grobovskiy | Personal Portfolio',
+  description: 'Alexander Grobovskiy is a React Developer with 4 years of experience.',
+}
+
+export default function RootLayout({
+                                     children,
+                                   }: {
+  children: React.ReactNode
+}) {
+  return (<html lang="en">
+    <body className={`${inter.className} relative bg-gray-50 text-gray-950`}>
+      <div
+        className="bg-[#fbe2e3] absolute -z-10 top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+      <div
+        className="bg-[#dbd7fb] absolute -z-10 top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+      {children}
+    </body>
+    </html>)
+}
