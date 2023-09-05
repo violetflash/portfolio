1f112668a324f1e8a40c022235c57976ccd0d9c1
Alex Grobovsky <a.grobovsky@gmail.com>
2023-09-05 23:10:43 +0500

    projects section process

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,5 +1,6 @@
 // @flow
 import { SectionHeading } from '@/components/section-heading';
+import { projectsData } from '@/lib/data';
 import * as React from 'react';
 
 type Props = {
@@ -10,8 +11,25 @@ export const Projects = (props: Props) => {
     <section>
       <SectionHeading text="Projects" />
       <div>
-
+        {
+          projectsData.map((project, index) => (
+            <Project key={index} {...project} />
+          ))
+        }
       </div>
     </section>
+  );
+};
+
+
+type ProjectProps = {
+  project: keyof typeof projectsData;
+};
+export const Project = (props: ProjectProps) => {
+
+  return (
+    <div>
+
+    </div>
   );
 };
