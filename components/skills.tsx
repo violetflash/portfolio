8c22e337b42589d12d77b8db98293f58cd990198
Alexander <50662772+violetflash@users.noreply.github.com>
2023-09-10 23:27:01 +0500

    feature/add-skills (#5)
    animated skills secton
    
    Co-authored-by: Alex Grobovsky <a.grobovsky@gmail.com>

---
+++ components/skills.tsx
@@ -0,0 +1,56 @@
+'use client';
+// @flow
+import { SectionHeading } from '@/components/section-heading';
+import { useSectionInView } from '@/hooks/use-section-in-view';
+import { skillsData } from '@/lib/data';
+import { cn } from '@/utils/cn';
+import { Variants, motion } from 'framer-motion';
+import * as React from 'react';
+
+const fadeInAnimationVariants: Variants = {
+  initial: {
+    opacity: 0,
+    y: 100
+  },
+  animate: (index: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.05 * index
+    }
+  })
+}
+
+type Props = {
+
+};
+export const Skills = (props: Props) => {
+
+  const { ref } = useSectionInView({
+    threshold: 0.5,
+    sectionName: 'Skills'
+  })
+
+  return (
+    <section id="skills" ref={ref} className="scroll-mt-28">
+      <SectionHeading text="My skills" />
+      <ul className={cn('flex flex-wrap max-w-[46rem] gap-2')}>
+        {skillsData.map((skill, index) => (
+          <motion.li
+            key={skill}
+            className={cn('bg-white border border-black/10 px-2 py-2 rounded-lg')}
+            variants={fadeInAnimationVariants}
+            initial="initial"
+            whileInView="animate"
+            viewport={{
+              once: true
+            }}
+            custom={index}
+          >
+            {skill}
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  );
+};
