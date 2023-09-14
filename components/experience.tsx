9b0ae86dceaf5d4810e286048a83df0fccd000c2
Alex Grobovsky <a.grobovsky@gmail.com>
2023-09-14 22:30:13 +0500

    add experience section

--- components/experience.tsx
+++ components/experience.tsx
@@ -17,10 +17,30 @@ export const Experience = (props: Props) => {
       <VerticalTimeline>
         {experiencesData.map((item, index) => (
             <React.Fragment key={index}>
-              <VerticalTimelineElement className="vertical-timeline-element--work" icon={item.icon}>
-                <h3>{item.title}</h3>
-                <p>{item.location}</p>
-                <p>{item.description}</p>
+              <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                icon={item.icon}
+                contentStyle={{
+                  background: '#f3f4f6',
+                  boxShadow: 'none',
+                  border: '1px solid rgba(0, 0, 0, 0.05)',
+                  textAlign: 'left',
+                  padding: '1.3rem 2rem'
+                }}
+                iconStyle={{
+                  background: '#f3f4f6',
+                  // background: 'white',
+                  fontSize: '1.3rem'
+                }}
+                contentArrowStyle={{
+                  borderRight: '0.4rem solid #9ca3af',
+                }}
+                date={item.date}
+              >
+                <h3 className="font-semibold capitalize">{item.title}</h3>
+                <p className='font-normal !mt-0'>{item.location}</p>
+                <p className='!mt-1 !mb-3 !font-normal text-gray-700'>{item.description}</p>
+                {item.url && <a className='!mt-4 !font-normal text-gray-700' href={item.url} target="_blank">{item.url}</a>}
               </VerticalTimelineElement>
             </React.Fragment>
         ))}
